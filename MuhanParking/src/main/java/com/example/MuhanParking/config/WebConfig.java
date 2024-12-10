4d61c506813f5425c5deb542c90d24b3aac42478
k-sulim <joy06270@naver.com>
2024-12-10 12:47:13 +0900

    sulim

--- MuhanParking/src/main/java/com/example/MuhanParking/config/WebConfig.java
+++ MuhanParking/src/main/java/com/example/MuhanParking/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3306")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
--- MuhanParking/src/main/java/com/example/MuhanParking/controller/Usercontroller.java
+++
@@ -1,4 +0,0 @@
-package com.example.MuhanParking.controller;
-
-public class Usercontroller {
-}
